ca99e49f1d4ea5f9c906923d2139a416a73ead3d
sterrettJD <sterrettjd@protonmail.com>
2023-01-06 11:55:34 -0700

    create filter function

--- analysis/metaphlan_to_phyloseq_helper.R
+++ analysis/metaphlan_to_phyloseq_helper.R
@@ -1,3 +1,19 @@
+# grabs a certain level of taxonomic resolution from the metaphlan output
+# defaults to the max level of resolution
+filter_mphlan_by_taxonomy_level <- function(df, level="max"){
+    tax.names <- rownames(df)
+    levels.contained <- stringr::str_count(tax.names, "\\|")
+    
+    if (level=="max"){
+        level <- max(levels.contained)
+    }
+    
+    to.return <- df[levels.contained==level,]
+    rownames(to.return) <- rownames(df)[levels.contained==level]
+    return (to.return)
+}
+
+
 # Converts metaphlan formatted taxonomy names to a taxonomy table for phyloseq
 # based on https://gist.github.com/lwaldron/512d1925a8102e921f05c5b25de7ec94
 names_to_tax_table <- function(bugs){
