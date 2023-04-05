0c693323af72b117cf9e3a551d00176d59a6ebe8
John Sterrett <sterrettJD@protonmail.com>
2023-04-05 16:22:26 -0600

    add funcs to reformat output

--- utils/CheckM_out_to_csv.py
+++ utils/CheckM_out_to_csv.py
@@ -19,13 +19,24 @@ def get_args():
     parsed_args = parser.parse_args()
     return parsed_args
 
+def replace_bin_id_with_filepath(df):
+    new_df = df.copy()
+    new_df["Bin Id"] = new_df["Bin Id"].apply(lambda x: "../hiv.t32.n40.metaspades.metabat2/bins_to_derep/" + x + ".fa")
+    return df
+
+def rename_columns(df):
+    return df.rename(columns={"Bin id": "genome",
+                              "Completeness": "completeness",
+                              "Contamination": "contamination",
+                              "Strain heterogeneity": "heterogeneity"})
 
 def main():
     args = get_args()
     # weird delimiter of at least 2 spaces seems to be working. It's not a tab, and some entries contain spaces
     df = pd.read_csv(args.infile, skipfooter=2, delimiter=r"\s\s+", engine="python")
-
     df.drop(0, inplace=True)
+    df = replace_bin_id_with_filepath(df)
+    df = rename_columns(df)
     df.to_csv(args.outfile, index=False)
 
 if __name__=="__main__":
