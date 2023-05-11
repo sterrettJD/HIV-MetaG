3f4586d77e61c994af1aa6298099c76235ab3d95
John Sterrett <sterrettJD@protonmail.com>
2023-05-11 15:26:47 -0600

    update aggregate metaphlan bugslists with padder capable version

--- utils/aggregate_metaphlan_bugslists.py
+++ utils/aggregate_metaphlan_bugslists.py
@@ -13,25 +13,33 @@ def get_args():
         prog="Aggregate Metaphlan bugs lists",
         description="Aggregates metaphlan bugs lists from humann outputs for multiple samples into a single tsv"
     )
-
+    # input directory
     parser.add_argument("-i", "--indir",
+                        help="Input directory that contains all of the sample directories",
                         required=True)
+    # a "padder" string that exists between the sample id and the metaphlan output name type
+    parser.add_argument("-p", "--padder",
+                        help="a 'padder' string that exists between the sample id and the humann output name type",
+                        default="")
+    # Output file path
     parser.add_argument("-o", "--outfile",
+                        help="Output file path",
                         required=True)
 
     parsed_args = parser.parse_args()
     return parsed_args
 
 
-def get_filepaths(directory):
+def get_filepaths(directory, padder):
     # get list of only subdirectories
-    subdirs = [d for d in os.listdir(directory) if os.path.isdir(d)]
-    # assumes each of the subdirectories is named for a sample id,
+    print(f"Searching {directory}")
+    subdirs = [d for d in os.listdir(directory) 
+               if os.path.isdir(os.path.join(directory, d))]
     # and each of these contains a subdir sampleid.concat_humann_temp
     # and each of the bugs list is named sampleid.concat_metaphlan_bugs_list.tsv
-    filepaths = [os.path.join(sampid,
-                              f"{sampid}.concat_humann_temp",
-                              f"{sampid}.concat_metaphlan_bugs_list.tsv") for sampid in subdirs]
+    filepaths = [os.path.join(directory, sampid,
+                              f"{sampid}{padder}_humann_temp",
+                              f"{sampid}{padder}_metaphlan_bugs_list.tsv") for sampid in subdirs]
     return filepaths, subdirs
 
 
@@ -54,7 +62,12 @@ def concat_files(filepaths_list, sampids_list):
 if __name__ == "__main__":
     args = get_args()
 
-    filepaths_list, sampids_list = get_filepaths(args.indir)
+    filepaths_list, sampids_list = get_filepaths(args.indir,
+                                                 padder=args.padder)
+    print("Aggregating the following files: ")
+    for filepath in filepaths_list:
+        print(filepath)
+    
     full = concat_files(filepaths_list, sampids_list)
 
     full.to_csv(f"{args.outfile}", sep="\t")
